441b5f2dd894b8ceb54631e637c66f70c401e3e1
Seamile <lanhuermao@gmail.com>
2019-12-27 09:54:57 +0800

    添加了点赞的关系表，做了数据库迁移

--- src/weibo/models.py
+++ src/weibo/models.py
@@ -11,3 +11,10 @@ class Weibo(db.Model):
     content = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now)  # 发布时间
     updated = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)  # 最后修改的时间
+
+
+class Like(db.Model):
+    __tablename__ = 'like'
+
+    uid = db.Column(db.Integer, primary_key=True)
+    wid = db.Column(db.Integer, primary_key=True)
